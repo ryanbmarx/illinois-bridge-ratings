3f038e22605388d892b711763f1ee5290d572473
ryanbmarx <ryanbmarx@gmail.com>
2017-03-05 00:54:12 -0600

    We have an ai2html blurb

--- tarbell_config.py
+++ tarbell_config.py
@@ -8,7 +8,7 @@ Tarbell project configuration
 SPREADSHEET_KEY = "1Qkg9kkfssUHspeltPjSr5fuMy6F40hIlDOOgDJjbhvc"
 
 # Exclude these files from publication
-EXCLUDES = ['*.md', 'requirements.txt', 'node_modules', 'sass', 'js/src', 'package.json', 'Gruntfile.js']
+EXCLUDES = ['*.ai', '*.md', 'requirements.txt', 'node_modules', 'sass', 'js/src', 'package.json', 'Gruntfile.js']
 
 # Spreadsheet cache lifetime in seconds. (Default: 4)
 # SPREADSHEET_CACHE_TTL = 4
